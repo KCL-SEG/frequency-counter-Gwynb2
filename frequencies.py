6dafb3a0d29eac3b2a8070e67d50de1299e50a60
Gwyn <gwyn.barrientos@kcl.ac.uk>
2022-10-23 08:59:40 +0100

    frequency function

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,18 @@
 
 def frequencies(items):
     frequencies = {}
+    for item in items:
+        if type(item) == int:
+            s =str(item)
+            if s in frequencies:
+                frequencies[s] += 1
+            else:
+                frequencies[s] = 1
+        else:
+            if item in frequencies:
+                frequencies[item] += 1
+            else:
+                frequencies[item] = 1
     # Your code goes here
     return frequencies
+list  = ["yes","yes","no",1,2,2,2]
---
+++ test.py
@@ -0,0 +1,10 @@
+x = 5
+def function():
+    if type(x)==str:
+         print("works")
+    else:
+        x2 = str(x)
+        print(type(x2))
+
+
+function()
